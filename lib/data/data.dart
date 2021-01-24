3aaccc4f9e6ce3661bd72ea128a385ef1b8b7e3e
Elen <elenneg23@gmail.com>
2021-01-24 23:33:44 +0200

    update home

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -6,20 +6,20 @@ List<CategoriesModel> getCategories(){
   List<CategoriesModel> categories = new List() ;
   CategoriesModel categoriesModel = new CategoriesModel();
 
-categoriesModel.imgUrl ='https://jenmulligandesign.com/wp-content/uploads/2017/04/pexels-beach-tropical-scene-free-stock-photo.jpg';
+categoriesModel.imgUrl ="https://c.files.bbci.co.uk/41CF/production/_109474861_angrycat-index-getty3-3.jpg";
 categoriesModel.categoriesName = 'cats';
 categories.add(categoriesModel);
 categoriesModel = new CategoriesModel();
 
-  categoriesModel.imgUrl ='https://jenmulligandesign.com/wp-content/uploads/2017/04/pexels-beach-tropical-scene-free-stock-photo.jpg';
+  categoriesModel.imgUrl ="https://c.files.bbci.co.uk/41CF/production/_109474861_angrycat-index-getty3-3.jpg";
   categoriesModel.categoriesName = 'cats2';
   categories.add(categoriesModel);
 
-  categoriesModel.imgUrl ='https://jenmulligandesign.com/wp-content/uploads/2017/04/pexels-beach-tropical-scene-free-stock-photo.jpg';
+  categoriesModel.imgUrl ="https://c.files.bbci.co.uk/41CF/production/_109474861_angrycat-index-getty3-3.jpg";
   categoriesModel.categoriesName = 'cats3';
   categories.add(categoriesModel);
 
-  categoriesModel.imgUrl ='https://jenmulligandesign.com/wp-content/uploads/2017/04/pexels-beach-tropical-scene-free-stock-photo.jpg';
+  categoriesModel.imgUrl ="https://c.files.bbci.co.uk/41CF/production/_109474861_angrycat-index-getty3-3.jpg";
   categoriesModel.categoriesName = 'cats4';
   categories.add(categoriesModel);
 
